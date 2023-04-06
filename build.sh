8a41a5cfd4199171995508077bb7cfe3e1ff9320
DerEingerostete <git@dereingerostete.dev>
2023-04-06 20:15:06 +0200

    Get version automatically

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Configurations
-folder="build"
-version="1.0.1"
-outputName="StreamPi-Hue-$version"
-
 # Moves a build to the target destination
 function moveBuild {
   mv "$1/target/$2-$version.jar" "$folder/$3.jar"
@@ -12,8 +7,17 @@ function moveBuild {
 
 function downloadDependency {
   echo "Downloading dependency $2"
-  curl --silent --remote-name --output-dir $folder "https://repo1.maven.org/maven2/$1/$2/$3/$2-$3.jar"
+  curl --silent --remote-name --output-dir "$folder" "https://repo1.maven.org/maven2/$1/$2/$3/$2-$3.jar"
 }
+
+function getProjectVersion {
+  grep -m 1 "<version>" pom.xml | sed -E 's/.*>(.+)<.*/\1/'
+}
+
+# Configurations
+folder="build"
+version=$(getProjectVersion)
+outputName="StreamPi-Hue-$version"
 
 # Debug info
 echo "Building plugin"
@@ -55,15 +59,15 @@ echo
 
 # Create archives
 echo "Creating archives"
-find $folder -printf "%P\n" -type f -o -type l -o -type d | tar -czf $outputName.tar.gz --no-recursion -C $folder -T -
-zip -r -qq -j $outputName.zip $folder
+find $folder -printf "%P\n" -type f -o -type l -o -type d | tar -czf "$outputName.tar.gz" --no-recursion -C "$folder" -T -
+zip -r -qq -j "$outputName.zip" $folder
 
 # Generating Hash
 echo "Generating sha256 checksums"
 sha256sum -b "$outputName.zip" "$outputName.tar.gz" > "$folder/sha256.txt"
 
 # Moving files to output folder
-mv $outputName.tar.gz $folder
-mv $outputName.zip $folder
+mv "$outputName.tar.gz" $folder
+mv "$outputName.zip" $folder
 
 echo Done
