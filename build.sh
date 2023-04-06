f9b82e789208a1385b8e9ef50317814d43e12123
DerEingerostete <git@dereingerostete.dev>
2023-04-06 18:49:34 +0200

    Generate sha256 checksums

--- build.sh
+++ build.sh
@@ -7,35 +7,36 @@ outputName="StreamPi-Hue-$version"
 
 # Moves a build to the target destination
 function moveBuild {
-  mv "./$1/target/$2-$version.jar" "$folder/$3.jar"
+  mv "$1/target/$2-$version.jar" "$folder/$3.jar"
 }
 
 function downloadDependency {
   echo "Downloading dependency $2"
-  curl --silent --remote-name --output-dir $folder https://repo1.maven.org/maven2/$1/$2/$3/$2-$3.jar
+  curl --silent --remote-name --output-dir $folder "https://repo1.maven.org/maven2/$1/$2/$3/$2-$3.jar"
 }
+
+# Debug info
+echo "Building plugin"
+echo "Version: $version"
+echo
 
 # Install dependencies
 echo "Installing packages"
 sudo apt-get -qq install -y zip tar curl
 
 # Clear output folder
-echo "Current directory: $PWD"
 if [ -d "$folder" ]; then rm -rf $folder; fi
 mkdir $folder
-
-# Debug info
-echo "Folder set to $folder"
-echo "Version set to $version"
 
 # Downloading dependencies
 echo "Downloading dependencies"
 downloadDependency "io/github/zeroone3010" "yetanotherhueapi" "2.7.0"
 downloadDependency "com/fasterxml/jackson/core" "jackson-databind" "2.14.2"
 downloadDependency "org/jetbrains" "annotations" "24.0.1"
+echo
 
 # Build maven
-echo Building maven packages
+echo "Building maven packages"
 mvn -B -q package -DskipTests --file pom.xml
 
 # Moving files
@@ -50,11 +51,19 @@ moveBuild "HueSetScene" "hue-set-scene" "Hue-Set-Scene"
 # Cleanup
 echo "Run cleanup"
 mvn -q clean
+echo
 
 # Create archives
+echo "Creating archives"
 find $folder -printf "%P\n" -type f -o -type l -o -type d | tar -czf $outputName.tar.gz --no-recursion -C $folder -T -
 zip -r -qq -j $outputName.zip $folder
-mv $outputName.tar.gz build
-mv $outputName.zip build
+
+# Generating Hash
+echo "Generating sha256 checksums"
+sha256sum -b "$outputName.zip" "$outputName.tar.gz" > "$folder/sha256.txt"
+
+# Moving files to output folder
+mv $outputName.tar.gz $folder
+mv $outputName.zip $folder
 
 echo Done
